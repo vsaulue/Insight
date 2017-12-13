c434885e14aa38a2ff9913fcec1a3c6c4dac61e4
Vincent Saulue-Laborde <vincent.saulue@hotmail.fr>
2017-12-13 14:57:09 +0100

    Insight: integrate Graphic & Physic engines into the app.

--- src/main.cpp
+++ src/main.cpp
@@ -15,25 +15,43 @@
  * along with Insight.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <atomic>
+#include <chrono>
+#include <cstdint>
 #include <cstdlib>
+#include <iostream>
+#include <thread>
 
+#include "GraphicEngine.hpp"
+#include "PhysicEngine.hpp"
+#include "Universe.hpp"
 #include "ShellClass.hpp"
 #include "ShellInterpreter.hpp"
 
 class Insight : public ShellClass {
 private:
+    using timer = std::chrono::steady_clock;
+
+    PhysicEngine physicEngine;
+    GraphicEngine graphicEngine;
     ShellInterpreter interpreter;
 
+    std::atomic_bool isEngineRunning;
+    std::chrono::duration<std::int64_t, std::nano> renderPeriod;
 public:
 
     virtual ShellClass& getField(const std::string& fieldName) override {
-        if (fieldName == "interpreter") {
+        if (fieldName == "physicEngine") {
+            return physicEngine;
+        } else if (fieldName == "interpreter") {
             return interpreter;
         }
         return ShellClass::getField(fieldName);
     }
 
-    Insight() : interpreter(*this) {}
+    Insight() : graphicEngine(physicEngine), interpreter(*this), isEngineRunning(false), renderPeriod(std::chrono::nanoseconds(1000000000/60)) {
+        physicEngine.getUniverse().addObject(new PhysicalObject());
+    }
 
     virtual const std::string& getShellClassName() const override {
         static const std::string className("Insight");
@@ -46,6 +64,22 @@ public:
 
     void quit() {
         interpreter.quit();
+        isEngineRunning = false;
+    }
+
+    void runEngine() {
+        isEngineRunning = true;
+        while (isEngineRunning && graphicEngine.run()) {
+            auto start = timer::now();
+            physicEngine.integrate();
+            graphicEngine.doRender();
+            auto ellapsed = timer::now() - start;
+
+            if (ellapsed < renderPeriod) {
+                auto sleepTime = renderPeriod - ellapsed;
+                std::this_thread::sleep_for(sleepTime);
+            }
+        }
     }
 
     void evalMethod(const std::string& methodName) override {
@@ -67,7 +101,11 @@ static void runInterpreter(Insight &insight) {
 int main(int argc, char** argv) {
     Insight insight;
 
-    runInterpreter(insight);
+    std::thread shellThread(runInterpreter, std::ref(insight));
+
+    insight.runEngine();
+
+    shellThread.join();
 
     return EXIT_SUCCESS;
 }
